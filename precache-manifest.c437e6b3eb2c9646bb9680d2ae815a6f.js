9bd9c0f1771609d0a3bc1fbd7a9adad8056633c9
cww <zn_caiww@corp.21cn.com>
2020-06-07 16:15:03 +0800

    ✨feat(all): 新建网站导航

---
+++ precache-manifest.c437e6b3eb2c9646bb9680d2ae815a6f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbdf1819da85c5a0305dd5066e76bf65",
+    "url": "/2s-tool/index.html"
+  },
+  {
+    "revision": "4664a4f95b9122fbc64e",
+    "url": "/2s-tool/static/css/main.e1567168.chunk.css"
+  },
+  {
+    "revision": "3dfda37076cd315e0a71",
+    "url": "/2s-tool/static/js/2.995fe95d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2s-tool/static/js/2.995fe95d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4664a4f95b9122fbc64e",
+    "url": "/2s-tool/static/js/main.1893b186.chunk.js"
+  },
+  {
+    "revision": "f48dbeb6afd762ad6e8e",
+    "url": "/2s-tool/static/js/runtime-main.3c1ad41e.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/2s-tool/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/2s-tool/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/2s-tool/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/2s-tool/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "2298261fe887af6d6893824b2da61f95",
+    "url": "/2s-tool/static/media/i.2298261f.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/2s-tool/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "379d7c494e0a91606359b63f853a98a0",
+    "url": "/2s-tool/static/media/qrcode-alipay.379d7c49.png"
+  },
+  {
+    "revision": "c3c016674caf75d53f171efb7742624c",
+    "url": "/2s-tool/static/media/qrcode-wechat.c3c01667.png"
+  }
+]);
